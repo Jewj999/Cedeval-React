07467c5328a900967cfbccce3c2841a63deb9227
oscarquintanilla <oscar.david.quintanilla@gmail.com>
2022-08-12 14:42:58 -0600

    #7 dashboard fixed #8 first login

--- src/pages/dashboard/components/Sidebar.tsx
+++ src/pages/dashboard/components/Sidebar.tsx
@@ -1,6 +1,4 @@
-import { userAtom } from '@src/atoms/user';
 import { FC } from 'react';
-import { useRecoilState } from 'recoil';
 import AccountSection from './AccountSection';
 import BeneficiarySection from './BeneficiarySection';
 
